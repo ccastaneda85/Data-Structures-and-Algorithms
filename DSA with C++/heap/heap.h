0517be0fae81970a8f9fc10eaec9c2ac01870cae
ccastaneda85 <christopherj.castaneda@gmail.com>
2024-02-28 08:38:19 -0800

    changes

--- DSA with C++/heap/heap.h
+++ DSA with C++/heap/heap.h
@@ -2,11 +2,15 @@
 #define HEAP_H
 
 template <typename T>
-class Heap
-{
+class Heap {
 public:
     virtual ~Heap() {}
-    virtual insert(const &T item) = 0;
+    virtual bool insert(const &T item) = 0;
+    virtual int getNumberofNodes() const = 0;
+    virtual int getheight() const = 0;
+    virtual T peekTop() const = 0;
+    virtual bool remove() = 0;
+    virtual void clear() = 0;
 };
 
 #endif
